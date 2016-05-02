e6ce4972af40ec3d802038a9b940c325c2b7d611
Jorge Aparicio <japaricious@gmail.com>
2016-05-02 15:05:00 -0500

    set the env vars in the right place

--- ci.sh
+++ ci.sh
@@ -9,11 +9,17 @@ main() {
     # issue, make everything world write-able.
     chmod -R a+w .
 
-    docker run -v $(pwd):/mnt -w /mnt japaric/copper:$tag bash -ex -c '
-        rustup default nightly
-        xargo build --release --verbose
-        arm-none-eabi-size $(find target/thumbv7m-none-eabi/release -maxdepth 1 -type f -executable)
-        arm-none-eabi-objdump -CD $(find target/thumbv7m-none-eabi/release -maxdepth 1 -type f -executable)
+    docker run \
+           -v $(pwd):/mnt \
+           -w /mnt \
+           -e AR_thumbv7m_none_eabi=arm-none-eabi-ar \
+           -e CC_thumbv7m_none_eabi=arm-none-eabi-gcc \
+           japaric/copper:$tag \
+           bash -ex -c '
+rustup default nightly
+xargo build --release --verbose
+arm-none-eabi-size $(find target/thumbv7m-none-eabi/release -maxdepth 1 -type f -executable)
+arm-none-eabi-objdump -CD $(find target/thumbv7m-none-eabi/release -maxdepth 1 -type f -executable)
     '
 }
 
