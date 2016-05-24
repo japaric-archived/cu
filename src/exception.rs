4df5a75b7d230577ce42a9a99e5b3123eeb7adc2
Jorge Aparicio <japaricious@gmail.com>
2016-05-23 22:53:46 -0500

    directly set the initial SP value in the linker script
    instead of setting it in a crate. This is more robust as the linker, AFAICT,
    won't remove the symbol this way.

--- src/exception.rs
+++ src/exception.rs
@@ -3,8 +3,6 @@
 use asm;
 
 extern "C" {
-    /// The start of the stack
-    static __STACK_START: u32;
     /// Reset
     pub fn start() -> !;
     /// Non-maskable interrupt
@@ -18,11 +16,6 @@ extern "C" {
 pub unsafe extern "C" fn __default_handler() {
     asm::bkpt();
 }
-
-/// Points at the start of the stack. This is used as the initial value of the stack pointer.
-#[link_section = ".stack_pointer"]
-#[no_mangle]
-pub static __STACK_POINTER: &'static u32 = &__STACK_START;
 
 /// Program entry point: The reset function.
 #[link_section = ".reset"]
